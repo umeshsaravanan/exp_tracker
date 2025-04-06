a6ab9a51e3722fdaa8fd77c6f1f4de18de76a4e7
umeshsaravanan <umeshsaravanan13@gmail.com>
2025-04-06 20:03:33 +0530

    handled route forwarding

--- src/main/java/com/expenseTracker/App.java
+++ src/main/java/com/expenseTracker/App.java
@@ -3,6 +3,7 @@ package com.expenseTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
@@ -20,6 +21,17 @@ public class App implements WebMvcConfigurer {
         .allowCredentials(true)
         .allowedMethods("GET", "POST", "PUT", "DELETE")  
         .allowedHeaders("*"); 
+    }
+    
+    // Route forwarding config for React
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/index.html");
+        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+                .setViewName("forward:/index.html");
     }
 }
 
